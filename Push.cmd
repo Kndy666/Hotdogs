8a99e5d5e53d46faaaf4b620384186ccc6ecd0c7
Kndy666 <kndy_666@163.com>
2018-11-17 23:27:23 +0800

    Update2018年11月17日23时27分23秒

--- Push.cmd
+++ Push.cmd
@@ -1,6 +1,6 @@
 cd %cd%
 git init
-git checkout -b gh-pages
+git checkout -b master
 git add .
-git commit -am "Update"
-git push git@github.com:Kndy666/js_Essay.git gh-pages --force"
+git commit -am "Update%date:~0,4%年%date:~5,2%月%date:~8,2%日%time:~0,2%时%time:~3,2%分%time:~6,2%秒"
+git push git@github.com:Kndy666/js_Essay.git master --force
